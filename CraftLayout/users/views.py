e687f82b2bd52074717158386e63c280da432785
Glebegor <glebegor2220@gmail.com>
2022-05-05 11:47:19 +0200

    Fixed buggs with Login and Reg. Also added errors to forms fields

--- CraftLayout/users/views.py
+++ CraftLayout/users/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
 from .forms import ProfileImage, FormRegister, ProfileBanner, FirstProfileform, SecondProfileform
 from exchange.models import Exchange
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 def RegisterPage(request):
     if request.method == "POST":
         form = FormRegister(request.POST)
         if form.is_valid():
             form.save()
+            username  = form.cleaned_data.get("username")
+            password  = form.cleaned_data.get("password1")
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
             return redirect('login')
     else:
         form = FormRegister()
     return render(request, 'users/reg.html', {'form':form})
-
-
-
 
 def profile(request):
     ExchangeOrd = Exchange.objects.filter(user=request.user)
